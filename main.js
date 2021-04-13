29842349716b5a4a036e9a975c23de84804425b1
DavidCodes987123 <75763282+DavidCodes987123@users.noreply.github.com>
2021-04-13 17:13:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,15 +7,23 @@ recognition.onresult=function(event) {
     console.log(event);
     var content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
- speak();
+    if (content=="take my selfie") {console.log("Taking your selfie!");
+ speak();}
 }
 function speak() {
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value; 
+    speak_data="Taking your selfie within five seconds!";
     var utterthis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterthis);
     Webcam.attach(camera);
+     setTimeout(function () {takeSnapshot(); save();}, 5000); 
 }
 Webcam.set({width:360, height:270,image_format:'png', png_quality:90});
 camera=document.getElementById("camera");
-
+function takeSnapshot() {Webcam.snap(function (data_uri) {
+    document.getElementById("result").innerHTML="<img id='selfie_image' src="+data_uri+">";
+});}
+function save() {link=document.getElementById("link")
+image=document.getElementById("selfie_image").src;
+link.href=image;
+link.click();}
